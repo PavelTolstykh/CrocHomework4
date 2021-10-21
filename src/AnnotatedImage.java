60bf42fcfddeccc13ee87a39db0a29ee94492b77
PavelTolstykh <55558945+PavelTolstykh@users.noreply.github.com>
2021-10-21 17:39:35 +0300

    Update AnnotatedImage.java

--- src/AnnotatedImage.java
+++ src/AnnotatedImage.java
@@ -16,5 +16,31 @@ class AnnotatedImage {
     public Annotation[] getAnnotations() {
         return this.annotations;
     }
+
+    public Annotation findByPoint(int x, int y) {
+        for (Annotation a : annotations) {
+            Figure fig = a.getFigure();
+            if (fig instanceof Circle && ((Circle) fig).getX0() - ((Circle) fig).getR() < x &&
+                    x < ((Circle) fig).getX0() + ((Circle) fig).getR() && ((Circle) fig).getY0() -
+                    ((Circle) fig).getR() < y && y < ((Circle) fig).getY0() + ((Circle) fig).getR()) {
+                return a;
+            } else if (fig instanceof Rectangle && ((Rectangle) fig).getX1() < x && x < ((Rectangle) fig).getX2() &&
+                    ((Rectangle) fig).getY1() < y && y < ((Rectangle) fig).getY2()) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public Annotation findByLabel(String label) {
+        for (Annotation a : annotations) {
+            Figure fig = a.getFigure();
+            if ((fig instanceof Circle || fig instanceof  Rectangle) && (((Circle)fig).getName().contains(label)
+                    || ((Rectangle)fig).getName().contains(label))) {
+                return a;
+            }
+        }
+        return null;
+    }
 }
 
